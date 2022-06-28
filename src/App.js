2e25724fbe1f71834f89c2c47c29f887073b1b92
emrahbeydilli <emrahbeydilli@gmail.com>
2022-06-28 20:53:16 +0300

    input detect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Rank from './components/rank/Rank';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
+class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      input:'',
+    }
+  }
+
+  onInputChange = (event) =>{
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () =>{
+    console.log('click');
+  }
+
+  render() { 
+    return (
+      <div className="App">
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm 
+      onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}/>
       {/* <FaceRecognition/> */}
     </div>
-  );
+    );
+  }
 }
-
+ 
 export default App;
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return ( 
         <div>
             <p className='f3'>
                 {'This Magic Brain will detect faces in your pictures. Give it a try!'}
             </p>
             <div className='w-70 center flex'>
-                <input className='f4 pa2 w-70 center' type="text" />
-                <button className='w-30 f4 white bg-green pointer'>Detect</button>
+                <input className='f4 pa2 w-70 center' type="text" onChange={onInputChange}/>
+                <button className='w-30 f4 white bg-green pointer' onClick={onButtonSubmit}>Detect</button>
             </div>
         </div>
      );
