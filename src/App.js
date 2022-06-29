058f0a5a739015fca526937c39b84cc53adbcaac
emrahbeydilli <emrahbeydilli@gmail.com>
2022-06-30 00:56:27 +0300

    face detection solved

--- src/App.js
+++ src/App.js
@@ -4,21 +4,47 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Rank from './components/rank/Rank';
+import FaceRecognition from './components/faceRecognition/FaceRecognition';
+import Clarifai from 'clarifai' ;
+ 
+const app = new Clarifai.App({
+ apiKey: '8119b9abd49940fd90ca443eba8479f4'
+});
 
 class App extends Component {
   constructor() {
     super();
     this.state ={
       input:'',
+      imageUrl: '',
+      box: {},
     }
   }
 
+  calculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    return{
+      leftCol: clarifaiFace.left_col*100,
+      topRow: clarifaiFace.top_row*100,
+      rightCol: 100 - (clarifaiFace.right_col*100),
+      bottomRow: 100 - (clarifaiFace.bottom_row*100)
+    }
+  }
+
+  displayFaceBox = (box) =>{
+    this.setState({box:box});
+  }
+
   onInputChange = (event) =>{
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () =>{
-    console.log('click');
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL,
+    this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err))
   }
 
   render() { 
@@ -30,7 +56,7 @@ class App extends Component {
       <ImageLinkForm 
       onInputChange={this.onInputChange} 
       onButtonSubmit={this.onButtonSubmit}/>
-      {/* <FaceRecognition/> */}
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
     </div>
     );
   }
---
+++ src/components/faceRecognition/FaceRecognition.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './FaceRecognition.css';
+
+const FaceRecognition = ({ imageUrl, box }) => {
+    console.log(box);
+    return (
+        <div className='flex justify-center mt3'>
+            <div className='absolute'>
+                <img id='inputimage' src={imageUrl} alt="" width='500px' height='auto'/>
+                <div className="bounding-box" style={{inset: `${box.topRow}% ${box.rightCol}% ${box.bottomRow}% ${box.leftCol}%`}}></div>
+            </div>
+        </div>
+    );
+}
+
+export default FaceRecognition;
